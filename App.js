180b58274b1691c6ab871825420ff0c025be9d6b
Duy Le <lehongduy.bk@gmail.com>
2023-02-07 18:03:31 +0700

    init commit

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import Page1 from './Page1';
+import Flex from './Page2';
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Page1}
+          options={{ title: 'Trang chu' }}
+        />
+        <Stack.Screen name="Flex" component={Flex} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
